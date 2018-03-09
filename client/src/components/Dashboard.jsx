e2f316ebcbda924a19ab65dd4a497b680a0904ce
unknown <suhail.purewal@gmail.com>
2018-03-09 11:54:40 -0600

    added packages to json

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
+import TopNav from "./components/TopNav";
 
 const Dashboard = ({ secretData, user }) => (
   <Card className="container">
+  <TopNav/>
     <CardTitle
       title="Dashboard"
       subtitle="You should get access to this page only after authentication."
